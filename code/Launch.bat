245b2856840e618e5a14acfe3145c7f8c47529a9
Kibishi47 <moulin.emmanuel47@gmail.com>
2024-01-06 15:58:22 +0100

    change batch

--- code/Launch.bat
+++ code/Launch.bat
@@ -1,26 +1,5 @@
 @echo off
 set "chemin_fichier=C:\Users\naina\OneDrive\Images\nainaincam 2023\Bot\Pairing_Discord\code\main.py"
 
-:menu
-cls
-echo 1. Exécuter le fichier Python
-echo 2. Arrêter le fichier Python
-echo 3. Quitter
-set /p choix="Choisissez une option : "
-
-if "%choix%"=="1" (
-    py "%chemin_fichier%"
-    pause
-    goto menu
-) else if "%choix%"=="2" (
-    taskkill /f /im python.exe /fi "CommandLine like '%%%chemin_fichier%%%'" 2>nul
-    echo Fichier Python arrêté.
-    pause
-    goto menu
-) else if "%choix%"=="3" (
-    exit
-) else (
-    echo Choix non valide.
-    pause
-    goto menu
-)
+py "%chemin_fichier%"
+exit
